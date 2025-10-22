439eaca60774ea3cd71c9bd858354d492766fde3
Nouhaila Aziki <noaziki@e3r10p2.1337.ma>
2025-10-22 08:46:22 +0100

    ex02 in C++ - Module 03 is done

---
+++ CPP-Module-03/ex02/ScavTrap.hpp
@@ -0,0 +1,19 @@
+#ifndef SCAVTRAP_HPP
+# define SCAVTRAP_HPP
+
+#include "ClapTrap.hpp"
+#include <string>
+
+class ScavTrap : public ClapTrap {
+    public:
+        ScavTrap();
+        ScavTrap(const std::string& name_value);
+        ScavTrap(const ScavTrap& other);
+        ScavTrap& operator=(const ScavTrap& other);
+        ~ScavTrap();
+
+        void    attack(const std::string& target);
+        void    guardGate();
+};
+
+#endif
