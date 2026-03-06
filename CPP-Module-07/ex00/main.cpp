692443ec7002ac13dfe8574be9514ce490bc43b6
Nouhaila Aziki <noaziki@e1r5p8.1337.ma>
2026-03-06 17:43:08 +0000

    add the official test cases from the subject

--- CPP-Module-07/ex00/main.cpp
+++ CPP-Module-07/ex00/main.cpp
@@ -2,21 +2,39 @@
 
 int main()
 {
-    int a = 6;
-    int b = 8;
+    /**************** SUBJECT TEST CASES *****************/
+    int a = 2;
+    int b = 3;
 
-    std::cout << "Before swap:\n";
-    std::cout << "a = " << a << "\n";
-    std::cout << "b = " << b << "\n\n";
+    ::swap(a, b);
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+    std::cout << "min( a, b ) = " << ::min(a, b) << std::endl;
+    std::cout << "max( a, b ) = " << ::max(a, b) << std::endl;
 
-    swap(a, b);
+    std::string c = "chaine1";
+    std::string d = "chaine2";
 
-    std::cout << "After swap:\n";
-    std::cout << "a = " << a << "\n";
-    std::cout << "b = " << b << "\n\n";
+    ::swap(c, d);
+    std::cout << "c = " << c << ", d = " << d << std::endl;
+    std::cout << "min( c, d ) = " << ::min(c, d) << std::endl;
+    std::cout << "max( c, d ) = " << ::max(c, d) << std::endl;
 
-    std::cout << "min(a, b) = " << min(a, b) << "\n";
-    std::cout << "max(a, b) = " << max(a, b) << "\n";
+    /**************** PERSONAL TEST CASES ****************/
+    // int a = 6;
+    // int b = 8;
 
-    return 0;
+    // std::cout << "Before swap:\n";
+    // std::cout << "a = " << a << "\n";
+    // std::cout << "b = " << b << "\n\n";
+
+    // swap(a, b);
+
+    // std::cout << "After swap:\n";
+    // std::cout << "a = " << a << "\n";
+    // std::cout << "b = " << b << "\n\n";
+
+    // std::cout << "min(a, b) = " << min(a, b) << "\n";
+    // std::cout << "max(a, b) = " << max(a, b) << "\n";
+
+    return (0);
 }
